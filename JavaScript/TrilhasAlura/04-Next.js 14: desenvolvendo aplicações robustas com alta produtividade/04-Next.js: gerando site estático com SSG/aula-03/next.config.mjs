79e8baec06d02a781055b2c14fbe4f9be7e06f33
GmSoftware <gmsoftware@gutomelo.com>
2026-06-28 10:10:48 -0300

    up dos exercícios do curso de Next.js: gerando site estático com SSG

---
+++ JavaScript/TrilhasAlura/04-Next.js 14: desenvolvendo aplicações robustas com alta produtividade/04-Next.js: gerando site estático com SSG/aula-03/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
