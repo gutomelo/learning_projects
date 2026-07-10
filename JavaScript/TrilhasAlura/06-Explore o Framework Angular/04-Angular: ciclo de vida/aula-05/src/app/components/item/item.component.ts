e48d9c022d3b218e428fcc9e12b8c1fec2e555d2
GmSoftware <gmsoftware@gutomelo.com>
2026-07-10 10:01:26 -0300

    up dos exercicios do curso de Angular: ciclo de vida

---
+++ JavaScript/TrilhasAlura/06-Explore o Framework Angular/04-Angular: ciclo de vida/aula-05/src/app/components/item/item.component.ts
@@ -0,0 +1,35 @@
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
+
+  faPen = faPen;
+  faTrash = faTrash
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngOnChanges(){ }
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item);
+  }
+  deletarItem(){
+    console.log('Estão tentando me calar.')
+    this.emitindoIdParaDeletar.emit(this.item.id);
+  }
+  ngOnDestroy(){
+    console.log('Conseguiram me calar.')
+  }
+}
