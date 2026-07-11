75e170721e35293a6c45fa3ef4ab92d2439f0c1f
GmSoftware <gmsoftware@gutomelo.com>
2026-07-11 18:04:30 -0300

    up dos arquivos do curso de RxJS e Angular: programando de forma reativa

---
+++ JavaScript/TrilhasAlura/06-Explore o Framework Angular/05-RxJS e Angular: programando de forma reativa/aula-04/src/app/views/lista-livros/lista-livros.component.ts
@@ -0,0 +1,43 @@
+import { Subscription } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { LivroService } from 'src/app/service/livro.service';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+
+@Component({
+  selector: 'app-lista-livros',
+  templateUrl: './lista-livros.component.html',
+  styleUrls: ['./lista-livros.component.css']
+})
+export class ListaLivrosComponent implements OnDestroy{
+
+  listaLivros: Livro[];
+  campoBusca: string = ''
+  subscription: Subscription
+  livro: Livro
+
+  constructor(private service: LivroService) { }
+
+  buscarLivros() {
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items)
+      },
+      error: erro => console.error(erro),
+    }
+    )
+  }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }
+}
+
+
+
